d3c61622d9fe5194e2abfe204ed656bce88bb89e
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-04-04 18:35:42 -0400

    Update routing.ts

--- src/constants/routing.ts
+++ src/constants/routing.ts
@@ -185,9 +185,9 @@ export const COMMON_BASES: ChainCurrencyList = {
   //   CUSD_CELO_ALFAJORES,
   //   CEUR_CELO_ALFAJORES,
   // ],
-  [SupportedChainId.SCROLL_ALPHA]: [
-    nativeOnChain(SupportedChainId.SCROLL_ALPHA),
-    WRAPPED_NATIVE_CURRENCY[SupportedChainId.SCROLL_ALPHA] as Token,
+  [SupportedChainId.KAVA]: [
+    nativeOnChain(SupportedChainId.KAVA),
+    WRAPPED_NATIVE_CURRENCY[SupportedChainId.KAVA] as Token,
   ],
 }
 
