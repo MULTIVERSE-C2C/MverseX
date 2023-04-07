d2d80becb53e0a3b762e74c284ea034a09b59434
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-04-07 10:31:39 -0400

    Update relay.config.js

--- relay.config.js
+++ relay.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   src: './src',
   language: 'typescript',
-  schema: './src/graphql/data/schema.graphql',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**', '**/thegraph/**'],
 }
