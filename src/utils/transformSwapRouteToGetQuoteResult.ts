63dd7128f360de7b440273a8cedd9fce026ad9a7
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-06-14 09:52:43 -0400

    Update transformSwapRouteToGetQuoteResult.ts

--- src/utils/transformSwapRouteToGetQuoteResult.ts
+++ src/utils/transformSwapRouteToGetQuoteResult.ts
@@ -1,7 +1,7 @@
 import { Protocol } from '@uniswap/router-sdk'
 import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 import { routeAmountsToString, SwapRoute } from '@uniswap/smart-order-router'
-import { Pool } from '@uniswap/v3-sdk'
+import { Pool } from '@dc1994/v3-sdk'
 import { GetQuoteResult, V2PoolInRoute, V3PoolInRoute } from 'state/routing/types'
 
 // from routing-api (https://github.com/Uniswap/routing-api/blob/main/lib/handlers/quote/quote.ts#L243-L311)
