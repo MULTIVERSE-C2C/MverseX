e8f5600e4119f3cbeae3e3569075ca3735e51003
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-06-14 09:31:57 -0400

    Update useAllV3Routes.ts

--- src/hooks/useAllV3Routes.ts
+++ src/hooks/useAllV3Routes.ts
@@ -1,5 +1,5 @@
 import { Currency } from '@uniswap/sdk-core'
-import { Pool, Route } from '@uniswap/v3-sdk'
+import { Pool, Route } from '@dc1994/v3-sdk'
 import { useWeb3React } from '@web3-react/core'
 import { useMemo } from 'react'
 
