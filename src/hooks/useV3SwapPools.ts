0ecd69268267659e181c70342f6ca9c6595cfdcf
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-06-14 09:43:01 -0400

    Update useV3SwapPools.ts

--- src/hooks/useV3SwapPools.ts
+++ src/hooks/useV3SwapPools.ts
@@ -1,5 +1,5 @@
 import { Currency, Token } from '@uniswap/sdk-core'
-import { FeeAmount, Pool } from '@uniswap/v3-sdk'
+import { FeeAmount, Pool } from '@dc1994/v3-sdk'
 import { useWeb3React } from '@web3-react/core'
 import { SupportedChainId } from 'constants/chains'
 import { useMemo } from 'react'
