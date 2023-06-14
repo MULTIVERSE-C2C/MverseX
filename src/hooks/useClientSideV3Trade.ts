72689c7ed870c9083b91d68551086db8d80a71e8
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2023-06-14 09:33:38 -0400

    Update useClientSideV3Trade.ts

--- src/hooks/useClientSideV3Trade.ts
+++ src/hooks/useClientSideV3Trade.ts
@@ -1,5 +1,5 @@
 import { Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
-import { Route, SwapQuoter } from '@uniswap/v3-sdk'
+import { Route, SwapQuoter } from '@dc1994/v3-sdk'
 import { useWeb3React } from '@web3-react/core'
 import { SupportedChainId } from 'constants/chains'
 import JSBI from 'jsbi'
